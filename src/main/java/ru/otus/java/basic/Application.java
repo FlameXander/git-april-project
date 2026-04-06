d9e3df4ec4e7e42d4108c54e6f90f92cc32a85cc
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:59:03 +0300

    f2

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -12,7 +12,8 @@ public class Application {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
-        System.out.println("-----");
+        System.out.println(4);
+        System.out.println("-----11");
     }
 
     public static void demo2() {
