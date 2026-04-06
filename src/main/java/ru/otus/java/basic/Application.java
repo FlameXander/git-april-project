7a4d420eb3a3e63dff11f5ad7031e34a543111fa
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:23:32 +0300

    Восстановили метод demo()

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -3,7 +3,16 @@ package ru.otus.java.basic;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        demo();
         demo2();
+    }
+
+    public static void demo() {
+        System.out.println("-----");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("-----");
     }
 
     public static void demo2() {
