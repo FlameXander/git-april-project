6c9f3f008a16ab502dd5fc4b1d8dfe259bcf3fc9
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:20:59 +0300

    Доработали метод demo до demo2

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -3,14 +3,14 @@ package ru.otus.java.basic;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        demo();
+        demo2();
     }
 
-    public static void demo() {
+    public static void demo2() {
         System.out.println("-----");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println("---" + 1);
+        System.out.println("---" + 2);
+        System.out.println("---" + 3);
         System.out.println("-----");
     }
 }
