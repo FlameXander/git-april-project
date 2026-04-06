b11591307337f7cbefd26bd84a65f68e237e215c
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:52:27 +0300

    demo3 modified

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -24,10 +24,10 @@ public class Application {
     }
 
     public static void demo3() {
-        System.out.println("-");
+        System.out.println("000-");
         System.out.println(1);
-        System.out.println(2);
+        System.out.println(200);
         System.out.println(3);
-        System.out.println("-");
+        System.out.println("000-");
     }
 }
