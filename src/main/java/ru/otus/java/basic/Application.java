4db5d1b6f57cc8856488adadb44a20f813c657f8
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:19:53 +0300

    Добавлен метод demo()

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -3,5 +3,14 @@ package ru.otus.java.basic;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        demo();
+    }
+
+    public static void demo() {
+        System.out.println("-----");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("-----");
     }
 }
