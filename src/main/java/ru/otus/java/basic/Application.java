ebdb4cfc2dc483259e38f9861b3c840a7b81e181
Fisunov Alexander <fisunov.a.v@gmail.com>
2026-04-06 20:24:35 +0300

    3 заменили на 8

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -19,7 +19,7 @@ public class Application {
         System.out.println("-----");
         System.out.println("---" + 1);
         System.out.println("---" + 2);
-        System.out.println("---" + 3);
+        System.out.println("---" + 8);
         System.out.println("-----");
     }
 }
